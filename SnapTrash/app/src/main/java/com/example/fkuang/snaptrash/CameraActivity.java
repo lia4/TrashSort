0ba6de67bf1ad35413b55411964f1798782e2041
foriskuang <forisk@uw.edu>
2016-10-15 23:53:43 -0700

    Stuff For Later

--- SnapTrash/app/src/main/java/com/example/fkuang/snaptrash/CameraActivity.java
+++ SnapTrash/app/src/main/java/com/example/fkuang/snaptrash/CameraActivity.java
@@ -1,13 +1,86 @@
 package com.example.fkuang.snaptrash;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
-public class CameraActivity extends AppCompatActivity {
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.hardware.Camera;
+import android.hardware.Camera.PictureCallback;
+import android.hardware.Camera.ShutterCallback;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnLongClickListener;
+import android.view.ViewGroup.LayoutParams;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class CameraActivity extends Activity {
+
+    private static final String TAG = "SnapTrash";
+    private static final int TAKE_PICTURE = 1;
+    private Uri imageUri;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+
+        Button cameraButton = (Button)findViewById(R.id.button_camera);
+        cameraButton.setOnClickListener(cameraListener);
     }
+
+    private void takePhoto(View v) {
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        File photo = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "picture.jpg");
+        imageUri = Uri.fromFile(photo);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        startActivityForResult(intent, TAKE_PICTURE);
+    }
+
+    private OnClickListener cameraListener = new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            takePhoto(v);
+        }
+    };
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        if(resultCode == Activity.RESULT_OK) {
+            Uri selectedImage = imageUri;
+            getContentResolver().notifyChange(selectedImage, null);
+
+            ImageView imageView = (ImageView)findViewById(R.id.image_camera);
+            ContentResolver cr = getContentResolver();
+            Bitmap bitmap;
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(cr, selectedImage);
+                imageView.setImageBitmap(bitmap);
+                Toast.makeText(CameraActivity.this, selectedImage.toString(), Toast.LENGTH_LONG).show();
+            } catch (Exception e) {
+                Log.e(TAG, e.toString());
+            }
+        }
+
+    }
+
 }
